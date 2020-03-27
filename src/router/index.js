9e45c8b1682322dbafef89801514875f68f71711
FoneQinrf <1164498684@qq.com>
2020-03-27 22:17:45 +0800

    am-ui

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 const router = new Router({
-  mode: 'history',
   routes: [
     {
         path: '/',
--- src/router/mobile.js
+++ src/router/mobile.js
@@ -5,7 +5,6 @@
     Vue.use(Router)
     
     const router = new Router({
-      mode: 'history',
         routes: [
             {
                 path: '/',
